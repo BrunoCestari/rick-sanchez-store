550989583219b9f9e1b0447f9db5b18735c408e2
Bruno Cestari <bccestari.project@gmail.com>
2024-11-14 18:39:58 -0300

    Add view controller method to handle invention creation with image upload and validation

--- src/main/java/com/bccestari/rick_sanchez_store/controllers/InventionsViewController.java
+++ src/main/java/com/bccestari/rick_sanchez_store/controllers/InventionsViewController.java
@@ -5,13 +5,25 @@ package com.bccestari.rick_sanchez_store.controllers;
 import com.bccestari.rick_sanchez_store.models.Invention;
 import com.bccestari.rick_sanchez_store.models.InventionDto;
 import com.bccestari.rick_sanchez_store.services.InventionsRepository;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.io.InputStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
+import java.util.Date;
 import java.util.List;
 @Controller
 @RequestMapping("/inventions")
@@ -32,6 +44,60 @@ public class InventionsViewController {
             model.addAttribute("inventionDto", inventionDto);
             return "inventions/CreateInvention";
         }
+        
+        
+    @PostMapping("/create")
+    public String createinvention(
+            @Valid @ModelAttribute InventionDto inventionDto,
+            BindingResult result
+    ) {
+        if (inventionDto.getImageFile().isEmpty()) {
+            result.addError(new FieldError("inventionDto", "imageFile", "The image file is required"));
+        }
+
+        if(result.hasErrors()){
+            return "inventions/CreateInvention";
+        }
+
+        // Save image file
+        MultipartFile image = inventionDto.getImageFile();
+        Date createdAt = new Date();
+        String storageFileName = createdAt.getTime() + "_" + image.getOriginalFilename();
+
+        try {
+            String uploadDir = "public/images/";
+            Path uploadPath = Paths.get(uploadDir);
+
+            if (!Files.exists(uploadPath)) {
+                Files.createDirectories(uploadPath);
+            }
+
+            try (InputStream inputStream = image.getInputStream()) {
+                Files.copy(inputStream, Paths.get(uploadDir + storageFileName),
+                        StandardCopyOption.REPLACE_EXISTING);
+            }
+        }    catch (Exception ex) {
+            System.out.println("Exception: " + ex.getMessage());
+        }
+
+
+        Invention invention = new Invention();
+        invention.setName(inventionDto.getName());
+        invention.setCategory(inventionDto.getCategory());
+        invention.setPrice(inventionDto.getPrice());
+        invention.setDescription(inventionDto.getDescription());
+        invention.setCreatedAt(createdAt);
+        invention.setImageFileName(storageFileName);
+
+        repo.save(invention);
+
+        return "redirect:/inventions";
+
+
+
+
+
+    }
 
 }
 
