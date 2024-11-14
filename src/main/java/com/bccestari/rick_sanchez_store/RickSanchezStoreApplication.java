4fa99c4960746e336a8b40d4eda84c08f66a86b9
Bruno Cestari <bccestari.project@gmail.com>
2024-11-13 23:39:13 -0300

    Initial commit: setup Spring Boot project

---
+++ src/main/java/com/bccestari/rick_sanchez_store/RickSanchezStoreApplication.java
@@ -0,0 +1,13 @@
+package com.bccestari.rick_sanchez_store;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RickSanchezStoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RickSanchezStoreApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bccestari/rick_sanchez_store/RickSanchezStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bccestari.rick_sanchez_store;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RickSanchezStoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
